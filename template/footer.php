f56e5a63d527f0ecac0ff166776e5d8117a9410c
mac <mac@MacBook-Pro-de-mac.local>
2018-07-15 23:28:53 +0200

    main cards added

--- template/footer.php
+++ template/footer.php
@@ -1,10 +1,17 @@
-<div class="card">
-  <div class="card-header">
-    Featured
+<footer>
+  <div class="card text-center">
+    <div class="card-header">
+        <p>
+          <?php
+          echo $author;
+          echo $date;
+          ?>
+        </p>
+    </div>
+    <div class="card-body">
+      <h5 class="card-title">Eshop Best Deals</h5>
+      <p class="card-text">Rejoignez notre communauté !</p>
+      <a href="#" class="btn btn-primary">Inscrivez-vous ici</a>
+    </div>
   </div>
-  <div class="card-body">
-    <h5 class="card-title">Eshop Best Deals</h5>
-    <p class="card-text">Rejoignez notre communauté !</p>
-    <a href="#" class="btn btn-primary">Inscrivez-vous ici</a>
-  </div>
-</div>
+</footer>
