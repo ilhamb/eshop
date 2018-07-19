17f4b61213beb62be054d4070bd0df7c5b94bb3e
mac <mac@MacBook-Pro-de-mac.local>
2018-07-19 15:30:16 +0200

    footer.php update

--- template/footer.php
+++ template/footer.php
@@ -1,17 +1,25 @@
-<footer>
-  <div class="card text-center">
-    <div class="card-header">
-        <p>
-          <?php
-          echo $author;
-          echo $date;
-          ?>
-        </p>
-    </div>
-    <div class="card-body">
-      <h5 class="card-title">Eshop Best Deals</h5>
-      <p class="card-text">Rejoignez notre communauté !</p>
-      <a href="#" class="btn btn-primary">Inscrivez-vous ici</a>
-    </div>
+<!-- Start of Footer -->
+<footer class="jumbotron jumbotron-fluid text-center">
+  <div class="container">
+  <p class="lead">Réalisation <?php echo $author ?></p>
   </div>
 </footer>
+<!-- End of Footer -->
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<script src="js/plugins.js"></script>
+<script src="js/main.js"></script>
+
+<!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+<script>
+    (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+    function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+    e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+    e.src='https://www.google-analytics.com/analytics.js';
+    r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+    ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+</script>
+</body>
+</html>
